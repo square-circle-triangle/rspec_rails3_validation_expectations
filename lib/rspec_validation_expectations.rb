cc49bfa463cded02063999b0f99c2351e7ba24cc
Jitendra Rai <jitendra.vinsol@gmail.com>
2011-01-10 04:30:14 +0530

    required rspec/expectations

--- lib/rspec_validation_expectations.rb
+++ lib/rspec_validation_expectations.rb
@@ -1,4 +1,5 @@
-require 'spec/rails/matchers/associations'
+require "rspec-expectations"
+require 'spec/associations'
 
 require 'rspec_validation_expectations/validation_expectations'
 require 'rspec_validation_expectations/association_expectations'
